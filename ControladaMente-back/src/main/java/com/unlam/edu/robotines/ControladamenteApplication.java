6b7d83e1b775b2d4ea9bdca4f415a434291e0836
CrisGadea <cristianhernangadea@gmail.com>
2024-04-11 15:23:37 -0300

    first commit back

---
+++ ControladaMente-back/src/main/java/com/unlam/edu/robotines/ControladamenteApplication.java
@@ -0,0 +1,13 @@
+package com.unlam.edu.robotines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControladamenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControladamenteApplication.class, args);
+	}
+
+}
---
+++ ControladaMente-back/src/test/java/com/unlam/edu/robotines/ControladamenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unlam.edu.robotines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControladamenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
