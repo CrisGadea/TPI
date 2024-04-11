9955dd7b7ce83af1ae073193ad742fca4c2f788c
CrisGadea <cristianhernangadea@gmail.com>
2024-04-11 17:27:02 -0300

    first components angular

--- ControladaMente-front/src/app/app.module.ts
+++ ControladaMente-front/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
